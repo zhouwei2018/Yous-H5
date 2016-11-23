f55d65ce6206e9ce4a5b563fb67d44ef0dfbdf34
zhouwei2018 <zhouwei163mail@163.com>
2016-11-23 11:13:43 +0800

    忘记密码 认证页面

---
+++ yous/view/forgetpwd.vue
@@ -0,0 +1,27 @@
+<style scoped>
+	.mui-input-row .mui-btn{width:auto;text-align:center;}
+	.mui-content{margin-top:10px;}
+</style>
+<template>
+	<header class="mui-bar mui-bar-nav">
+		    <a class="mui-action-back mui-icon mui-icon-left-nav mui-pull-left" onclick="window.history.go(-1)"></a>
+		    <h1 class="mui-title">忘记密码</h1>
+		</header>
+		<div class="mui-content">
+			<form class="mui-input-group">
+				<div class="mui-input-row">
+					<input id='telphone' type="number" class="mui-input-clear mui-input" placeholder="输入手机号">
+				</div>
+				<div class="mui-input-row" style='position:relative;'>
+					<input id='verificationCode' type="number" class="mui-input-clear mui-input" placeholder="输入验证码">	
+					<input type="button" value="获取验证码" style='background-color: #CCCCCC; position:absolute;right:10px;top:2px; width: auto' class="mui-btn getnumber">
+				</div>
+				<div class="mui-input-row">		
+					<input type="password" class="mui-input-clear mui-input" placeholder="新密码">					
+				</div>
+			</form>
+			<div class="mui-content-padded">
+				<button id='sendMail' class="mui-btn mui-btn-block" style='background:#ff8112;color:#fff;'>确认修改</button>
+			</div>
+		</div>
+</template>
---
+++ yous/view/user/apply.vue
@@ -0,0 +1,33 @@
+<style scoped>
+	label,input{color:#444;font-size:14px;}
+	.mui-button-row{margin-top:20px;}
+	.mui-input-group{padding-top:10px;}
+</style>
+
+
+<template>
+	<header class="mui-bar mui-bar-nav">
+	    <a class="mui-action-back mui-icon mui-icon-left-nav mui-pull-left" onclick="window.history.go(-1)"></a>
+	    <h1 class="mui-title">申请认证</h1>
+	</header>
+	<div class="mui-content">
+     <form class="mui-input-group">
+			<div class="mui-input-row">
+				<label>公司名称</label>
+				<input type="text" class="mui-input-clear" placeholder="请输入公司名称">
+			</div>
+			<div class="mui-input-row">
+				<label>公司LOGO</label>
+				<input type="text" class="mui-input-clear" placeholder="请上传公司LOGO">
+			</div>
+			<div class="mui-button-row">
+				<button type="button" class="mui-btn mui-btn-primary" onclick="return false;">确认</button>&nbsp;&nbsp;
+				<button type="button" class="mui-btn mui-btn-danger" onclick="return false;">取消</button>
+			</div>
+		</form>
+	</div>
+	
+	
+	
+	
+</template>
