716bf8870652e38ec20efa1f3cd7b315dbdb3455
zhouwei2018 <zhouwei163mail@163.com>
2016-11-24 18:46:28 +0800

    详情页面detail 进程页面process 评价aftersale 增加 均在order下面

---
+++ yous/view/user/order/detail.vue
@@ -0,0 +1,66 @@
+<style scoped>
+
+/*这里是详情页面*/
+
+
+
+	.zhuangtai{float:right;color:orangered;;margin-right:10%;}
+	p:last-child{margin-top:10px;font-size:13px;}
+	.mui-icon{font-size:16px;}
+	.address p{font-size:14px;}
+	.address p .mui-icon{margin-right:10px;}
+	.address p .tel{margin-left:10px;}
+	.mui-table-view{margin-top:5px;}
+	p{clear:both;}
+	.weixiu{margin-top:10px;background:#fff;padding:10px;overflow: hidden;}
+	.weixiu h4{font-size:15px;font-weight: normal;color:#666;}
+	.weixiutitle{font-size:14px;font-weight: normal;color:#aaa;height:20px;line-height:20px;margin-top:10px;}
+	.weixiubtn{height:20px;line-height:8px;font-size:10px;margin-left:10px;}
+	.weixiupic img{width:80px;height:80px;}
+	.peijian{border-bottom:1px solid #eee;padding-bottom:10px;}
+	.moneyspan{float:right;color:#f00;font-size:13px;}
+	.shiji{font-size:15px;float:right;color:#2C3637;}
+	.xiadan{float:right;margin-top:1px;}
+	
+	.shijispan{font-size:19px;color:#f00;}
+	.footer button{width:23%;font-size:13px;}
+	.footer{text-align:center;padding-bottom:10px;}
+	.footer,button{background:#fff}
+	.kefutel{background:#fff;border-color:#ddd}	
+</style>
+<template>
+		<div class="mui-content">
+			<!--头部的-->
+		    <ul class="mui-table-view">
+				<li class="mui-table-view-cell">					
+					<a class="mui-navigate-right" id='process'>
+						<p>订单编号:1213245<span class='zhuangtai'>已完结</span></p>
+						<p><span class="mui-icon mui-icon-home"></span>感谢使用幼狮服务</p>
+					</a>
+				</li>
+			</ul>
+			<!--地址的-->
+			<ul class="mui-table-view">
+				 <li class="mui-table-view-cell address">
+				 	<p><span class="mui-icon mui-icon-location-filled"></span>张三<span class='tel'>1232356465</span></p>
+				 	<p>北京市朝阳区共三环建外SOHOA座1503</p>
+				 </li>
+			</ul>
+			<div class='weixiu'>
+				<h4>设备维修</h4>
+				<p class='weixiutitle'>空调制冷维修<button type="button" class="mui-btn mui-btn-blue mui-btn-outlined weixiubtn">管件维修</button></p>
+				<p class='weixiupic'><img src="../../../../dist/img/iconfont-tianjia.png"><img src='../../../../dist/img/iconfont-tianjia.png'></p>
+				<p class='money'>服务金额<span class='moneyspan'>￥100.00</span></p>
+				<p class='peijian'>+配件费<span class='moneyspan'>￥0.00</span></p>
+				<p class='shiji'>实际付款:<span class='shijispan'>123.00</span></p>
+				<p class='xiadan'>下单时间:<span>1206-12-15 12:12:23</span></p>
+			</div>
+			<div class='footer'>
+				<a class="mui-btn mui-btn-block mui-btn-outlined kefutel" href='tel:312312'><span class="mui-icon mui-icon-phone" style='color:orangered'></span>13216546</a>		
+				<button type="button" class="mui-btn mui-btn-outlined">取消订单</button>
+				<button type="button" class="mui-btn mui-btn-outlined">删除订单</button>
+				<button type="button" class="mui-btn mui-btn-outlined">评价订单</button>
+				<button type="button" class="mui-btn mui-btn-outlined">投诉建议</button>
+			</div>
+		</div>	
+</template>
---
+++ yous/view/user/order/process.vue
@@ -0,0 +1,74 @@
+<style scoped>
+/*进程页面 有个背景图片无法添加*/
+
+	ul li{list-style: none;font-size:13px;}
+	.track-rcol{border: 1px solid #eee;background:#fff;padding-bottom:30px;}
+	.track-list{position: relative;}
+	.track-list li{position: relative; padding: 9px 0 0 15px; line-height: 18px; border-left: 1px solid #d9d9d9; color: #999;margin-top:20px;}
+	.track-list li.first{color: red; padding-top: 0; border-left-color: #fff;}
+	/*背景图片添加不上*/
+	.track-list li .node-icon{position: absolute; left: -6px; top: 50%; width: 11px; height: 11px; background: url(../../../../dist/img/order-icons.png)  -21px -72px no-repeat;}
+	.track-list li.first .node-icon{background-position:0 -72px;}
+	.track-list li .time{margin-right: 20px; position: relative; top: 4px; display: inline-block; vertical-align: middle;}
+	.track-list li .txt{ position: relative; top: 4px; display: inline-block; vertical-align: middle;}
+	.track-list li.first .time{margin-right: 20px; }
+	 ul{background:#fff;padding-top:10px;padding-bottom:10px;}
+	.list li{list-style-type:none;padding-left:10px;color:#555;font-size:14px;padding-right:20px;margin-top:10px;}
+	.list .zhuangtai{float:right;color:#f00;}
+	.list li .processcontent{margin-left:10px;}
+	li{margin-left:20px!important;}
+</style>
+
+<template >
+		<header class="mui-bar mui-bar-nav">
+		    <a class="mui-action-back mui-icon mui-icon-left-nav mui-pull-left"></a>
+		    <h1 class="mui-title">进程订单</h1>
+		</header>
+		<div class="mui-content">
+		    <ul class='list'>
+		    	<li>订单编号:<span class="processcontent">2016156655</span><span class='zhuangtai'>已完成</span></li>
+		    	<li>服务状态:<span class="processcontent">服务中</span></li>
+		    	<li>维修人员:<span class="processcontent">蔡国庆</span></li>
+		    	<li>联系电话:<a href='tel:132312'><span class="processcontent">123554469652</span></a></li>	
+		    </ul>		    
+		    <div class="track-rcol">
+			<div class="track-list">
+				<ul>
+					<li class="first">
+						<i class="node-icon"></i>
+						<span class="txt">感谢使用幼狮空间维修服务</span>
+					</li>
+					<li>
+						<i class="node-icon"></i>
+						<span class="time">2016-03-10 18:07:15</span><br>
+						<span class="txt">等待业主确认</span>
+					</li>
+					<li>
+						<i class="node-icon"></i>
+						<span class="time">2016-03-10 18:07:15</span><br>
+						<span class="txt">服务业经完成</span>
+					</li>
+					<li>
+						<i class="node-icon"></i>
+						<span class="time">2016-03-10 18:07:15</span><br>
+						<span class="txt">维修人员正在上门维修</span>
+					</li>
+					<li>
+						<i class="node-icon"></i>
+						<span class="time">2016-03-10 18:07:15</span><br>
+						<span class="txt">维修人员【蔡国庆】在在赶往维修地</span>
+					</li>
+					<li>
+						<i class="node-icon"></i>
+						<span class="time">2016-03-10 18:07:15</span><br>
+						<span class="txt">维修单已生成</span>
+					</li>
+				</ul>
+			</div>
+		</div>
+		    
+		</div>
+	
+	
+	
+</template>
