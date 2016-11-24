f9566705e4be9a653d8c9f95712a928609962b2c
zhouwei2018 <zhouwei163mail@163.com>
2016-11-24 17:35:32 +0800

    订单中心 样式

--- src/components/tabRouterPanel.vue
+++ src/components/tabRouterPanel.vue
@@ -1,7 +1,13 @@
-<style>
+<style scoped>
     .t-tab__bd{
-        display: block;
+        display: block;margin-top:0px;background:#fff;padding:0px;
     }
+    section {padding:0;}
+			.t-tab nav a{padding-top:2px;padding-bottom:2px;display:inline-block;
+			width:19%;text-align:center;border-right:1px solid #ddd;font-size:14px;color:#333;
+			border-bottom:2px solid transparent;background:#fff;}
+    .t-tab nav a:last-child{border:0;}
+    .t-tab nav a.active{border-bottom:2px solid orangered}
 </style>
 <template>
     <section class="t-tab">
@@ -9,7 +15,7 @@
             <a v-for="its in list" v-link="{ path: its.path}" v-text="its.title" ></a>
         </nav>
         <div class="t-tab__bd">
-              <router-view></router-view>
+            <router-view></router-view>
         </div>
     </section>
 </template>
