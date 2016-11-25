ffcfe7e45d36b530173be70e6dc26c278f0cbd15
zhouwei2018 <zhouwei163mail@163.com>
2016-11-25 17:45:01 +0800

    订单详情的划过效果

--- src/components/tabRouterPanel.vue
+++ src/components/tabRouterPanel.vue
@@ -8,12 +8,14 @@
     .t-tab nav a{
         padding-top:2px;padding-bottom:2px;display:inline-block;
         width:19%;text-align:center;border-right:1px solid #ddd;font-size:14px;color:#333;
-        border-bottom:2px solid transparent;
+        border-bottom:2px solid transparent;background-color:#fff;
     }
     .t-tab nav a:last-child{border:0;}
     .t-tab nav a.active{border-bottom:2px solid orangered}
 
-    .v-link-active{background-color: red}
+    .t-tab nav a.v-link-active{border-bottom:2px solid orangered;color:orangered}
+    .t-tab nav{position:fixed;left:0;z-index:10;}
+	.t-tab__bd{padding-top:50px;}
 </style>
 <template>
     <section class="t-tab">
