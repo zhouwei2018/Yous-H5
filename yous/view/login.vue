2b5803b825b91aac77eeb8a680a4524848f9d34f
zhouwei <zhouwei163mail@163.com>
2016-12-02 15:58:51 +0800

    还原登录和注册

--- yous/view/login.vue
+++ yous/view/login.vue
@@ -1,46 +1,13 @@
 <style scoped>
-	/*.spliter{color:#aaa;font-size:0.24rem;}
-	.link-area a{color:#333;font-size:0.28rem;}
-	.mui-content{padding-top:0.1rem;}*/	
-	.mui-content{width: 100%;height:100%;overflow: hidden;color: #ffffff; background-size: cover;padding-bottom:3rem;}
-	.mui-bar-nav {top: 0; -webkit-box-shadow: 0 0px 0px #ccc; box-shadow: 0 0px 0px #ccc; }
-			input::-webkit-input-placeholder,textarea::-webkit-input-placeholder {color: #aaa;font-size: .22rem;}
-			.backnone{background-color:transparent;}
-			.mui-input-row label{padding: 0px;margin-top: .32rem;}
-			.mui-input{width: 90%;margin:0 auto;background: none;margin-top: 2rem;}
-			.mui-input{border: none;}
-			.mui-input label{width: 100%;text-align: center;font-size: .4rem;}
-			.mui-input .mui-input-row{width: 100%;height:1.6rem;text-align: center;}
-			.mui-btn{width: 100%;height:1rem;border-radius:.3rem;margin-top: 0.6rem;font-size: .33rem;}
-			.mui-content-padded{text-align: center;font-size: .22rem;}
-			.mui-input-row input{width: 100%;text-align: center;color: #ffffff;}
-			.mui-input-row label ~ input, .mui-input-row label ~ select, .mui-input-row label ~ textarea{width: 100%;padding-right: 0;}
-			.xian{float:none;height:1px;background: #fff;opacity: 0.5;clear: both;}
+    .spliter{color:#aaa;font-size:0.24rem;}
+    .link-area a{color:#333;font-size:0.28rem;}
+    .mui-content{padding-top:0.1rem;}
 </style>
 <template>
-	<header class="mui-bar mui-bar-nav" style='background:transparent;color:#fff;'>
-	    <a class="mui-action-back mui-icon mui-icon-left-nav mui-pull-left" onclick="window.history.go(-1)" style='color:#fff;'></a>
-	    <h1 class="mui-title" style='color:#fff;'>登&nbsp;录</h1>
-	</header>
-	<div class="mui-content backnone" style='background:url(../../dist/img/loginbg.jpg) no-repeat;background-size: cover;'>
-		<form class="mui-input">
-		    <div class="mui-input-row">
-		        <label>手机号</label>
-		    	<input type="text" class="mui-input-clear" placeholder="请输入手机号" v-model="phone">
-		    	<div class="mui-pager xian"></div>
-		    </div>
-		    <div class="mui-input-row">
-		        <label>密码</label>
-		        <input type="password" class="mui-input-password" placeholder="请输入密码" v-model="pwd">
-		        <div class="mui-pager xian"></div>
-		    </div>
-		        <button type="button" class="mui-btn mui-btn-primary" v-on:click="asyLogin" style='background:#0f6fac'>登录</button>
-		     <div class="mui-content-padded">
-		       <span style='margin-right:0.5rem;' v-link="{ path: '/register'}">注册账号</span><span v-link="{ path: '/forgetpwd'}">忘记密码 ？</span>
-		    </div>
-		</form>
-	</div>
-    <!--<div class="mui-content">
+    <header1></header1>
+    <nav1></nav1>
+
+    <div class="mui-content">
         <header class="mui-bar mui-bar-nav">
             <a class="mui-action-back mui-icon mui-icon-left-nav mui-pull-left" onclick="window.history.go(-1)"></a>
             <h1 class="mui-title">登录</h1>
@@ -58,13 +25,18 @@
             <div class="link-area"><a v-link="{ path: '/register'}">注册账号</a> <span class="spliter">|</span> <a v-link="{ path: '/forgetpwd'}">忘记密码</a>
             </div>
         </div>
-    </div>-->
+    </div>
 
 </template>
 <script>
+    import header1 from './componets/header.vue'
+    import nav1 from './componets/nav.vue'
     import alert from '../../src/components/alert.vue'
+
     export default {
         components : {
+            header1,
+            nav1,
             alert,
         },
         data () {
@@ -120,7 +92,4 @@
         }
     }
     }
-    
-   
-
 </script>
