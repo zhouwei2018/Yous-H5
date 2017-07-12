fcb6dcd26dca4bb84ca23718bc7ece007ec0dfe1
zhouwei <zhouwei163mail@163.com>
2017-07-12 13:39:20 +0800

    修改端口

--- config/index.js
+++ config/index.js
@@ -23,7 +23,7 @@ module.exports = {
   },
   dev: {
     env: require('./dev.env'),
-    port: 9090,
+    port: 9091,
     autoOpenBrowser: true,
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
