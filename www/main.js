e30a9de48bc078b7ab341b0fabd80e8ba12aaf06
heyong <18810956240@163.com>
2017-07-15 10:03:21 +0800

    文件重新提交

--- www/main.js
+++ www/main.js
@@ -11,7 +11,6 @@ import search from './routers/search.vue';
 Vue.use(VueRouter);
 Vue.use(VueResource);
 Vue.use(MintUI);
-
 Vue.prototype.$api = "http://116.62.71.76:8001/api/GetServiceApiResult" //api地址
 Vue.prototype.$resouceUrl = "http://localhost:8081/"  //资源文件地址
 Vue.config.debug = true;// 开启debug模式
