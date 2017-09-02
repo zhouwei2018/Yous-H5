a00303a2b27e571887d1f39359940d72d7af3c2c
qizhenzhen2017@126.com <haiou920113>
2017-09-02 10:54:33 +0800

    去掉左侧登录

--- www/components/header.vue
+++ www/components/header.vue
@@ -8,9 +8,14 @@
     <!--header start-->
     <header id="header" class="clearfix">
 
-      <label class="side_nav side-nav" @click.native="popupVisible= true" v-on:click="showMenu">
+      <!--<label class="side_nav side-nav" @click.native="popupVisible= true" v-on:click="showMenu">-->
+        <!--<img src="../resources/images/ys_more.png" width="20" alt="">-->
+      <!--</label>-->
+
+      <label class="side_nav side-nav">
         <img src="../resources/images/ys_more.png" width="20" alt="">
       </label>
+
       <!-- <a href="javascript:void(0);" class="detail-search">
           <input type="text" id="keyword" placeholder="请输入关键字搜索" value="" maxlength="50">
       </a>
